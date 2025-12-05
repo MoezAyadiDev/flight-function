b8e6f789482df430ce5f2a5935033f5f9fc9e4d5
MoezAyadiDev <moez.ayadi.dev@gmail.com>
2025-12-05 18:22:29 +0100

    updateFlights

--- api/init.ts
+++ api/init.ts
@@ -1,8 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-
+import { initDatabaseService } from "../src/init.database.service";
 export default function handler(req: VercelRequest, res: VercelResponse) {
   //const { name = "World" } = req.query;
-  return res.json({
-    message: `Hello init!`,
-  });
+  return res.json(initDatabaseService());
 }
--- api/traffics.ts
+++ api/traffics.ts
@@ -1,8 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { flightService } from "../src/services/flight.service";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   //const { name = "World" } = req.query;
-  return res.json({
-    message: `Hello traffic!`,
-  });
+  return res.json(flightService());
 }
