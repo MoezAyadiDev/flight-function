a292191ae60d7bd058bc36b3d159f75f470b3639
MoezAyadiDev <moez.ayadi.dev@gmail.com>
2025-12-06 09:07:45 +0100

    updateheader2

--- src/utils/parse.request.ts
+++ src/utils/parse.request.ts
@@ -3,7 +3,9 @@ import "dotenv/config";
 import { AutorisationFailures } from "../types/failures";
 
 export function autorizationMiddleware(hed: any) {
+  console.log(hed);
   const autorisation = hed["functionkey"];
+  console.log("autorisation", autorisation);
   if (autorisation !== process.env.FUNCTION_KEY) {
     throw new AutorisationFailures();
   }
